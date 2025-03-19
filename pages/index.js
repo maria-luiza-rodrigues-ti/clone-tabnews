b19c47bb268ff6341bdff76f4f525e60c66e447a
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-03-19 22:20:39 +0000

    changing the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Estude sempre que puder!</h1>
+  return <h1>"Cada pequeno avanço nos estudos é um passo a mais em direção ao seu futuro. O conhecimento que você adquire hoje é a ferramenta que abrirá portas amanhã." 🚀📚</h1>
 }
 
 export default Home;
