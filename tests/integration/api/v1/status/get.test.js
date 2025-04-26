63048fa9f94a8018f39246a2cfe71a8bf3b75aeb
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-04-26 20:49:14 +0000

    feat: finishing `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,14 @@
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
-  expect(responseBody.updated_at).toBeDefined();
-
   const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
-
   expect(responseBody.updated_at).toBe(updatedAtParsed);
+
+  expect(responseBody.dependencies.version).toEqual("16.0")
+  expect(responseBody.dependencies.max_connections).toEqual(100)
+  expect(responseBody.dependencies.opened_connections).toEqual(1)
 });
