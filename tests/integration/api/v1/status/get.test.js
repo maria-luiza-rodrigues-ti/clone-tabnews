b79ca1a839349ff8dc0a4d5c3badeae171ca0b8c
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-05-15 15:48:05 +0000

    refact test `api/v1/migrations`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,3 @@
-
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
@@ -8,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(updatedAtParsed);
 
-  expect(responseBody.dependencies.version).toEqual("16.0")
-  expect(responseBody.dependencies.max_connections).toEqual(100)
-  expect(responseBody.dependencies.opened_connections).toEqual(1)
+  expect(responseBody.dependencies.version).toEqual("16.0");
+  expect(responseBody.dependencies.max_connections).toEqual(100);
+  expect(responseBody.dependencies.opened_connections).toEqual(1);
 });
