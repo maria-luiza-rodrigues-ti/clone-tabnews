fa93caa02f3ca8185f67c01cff9f4a20dd067d90
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-05-26 14:25:31 +0000

    fix `/migrations` endpoint connection bug

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,9 +3,7 @@ import database from "infra/database.js";
 beforeAll(cleanDatabase);
 
 async function cleanDatabase() {
-  return await database.query(
-    "drop schema public cascade; create schema public;",
-  );
+  await database.query("drop schema public cascade; create schema public;");
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
@@ -14,18 +12,18 @@ test("POST to /api/v1/migrations should return 200", async () => {
   });
   expect(response1.status).toBe(201);
 
-  const responseBody1 = await response1.json();
+  const response1Body = await response1.json();
 
-  expect(Array.isArray(responseBody1)).toBe(true);
-  expect(responseBody1.length).toBeGreaterThan(0);
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
 
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response2.status).toBe(200);
 
-  const responseBody2 = await response2.json();
+  const response2Body = await response2.json();
 
-  expect(Array.isArray(responseBody2)).toBe(true);
-  expect(responseBody2.length).toBe(0);
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
